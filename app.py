4512a646a201e0ea319d8ac81e03a942b9293948
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-22 18:37:54 +0530

    api works. model needs finetuning

--- app.py
+++ app.py
@@ -1,14 +1,64 @@
-from flask import Flask
+import json
+from datetime import datetime
+from flask import Flask, request
+
+import weatherAPI as api
+
+from keras.models import load_model
+import tensorflow
 
 app = Flask(__name__)
+model = load_model('MLModel/LoraxV1.0.h5')
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Lorax says hi'
 
-@app.route('/predict')
+
+"""
+request json structure
+{
+    "region": string,
+    "date": string, //YYYY-MM-DD
+    "temp": float,
+    "humidity": float    
+}
+"""
+
+
+@app.route('/predict', methods=['POST'])
 def predict():
+    req = request.get_json()
+    region = req['region']
+    date = req['date']
+    temp = req['temp']
+    humidity = req['humidity']
+
+    wind, rain = api.makeCall(region)
+    print(wind, rain)
+
+    dayoftheweek = datetime.strptime(date, '%Y-%m-%d').weekday() + 1
+    print(datetime.strptime(date, '%Y-%m-%d'))
+
+    year, month, day = date.split('-')
+    #month, day, temp, RH, wind, rain
+    print(int(month), dayoftheweek, temp, humidity, wind, rain)
+    data = [int(month), dayoftheweek, temp, humidity, wind, rain]
+    prediction = model.predict([data])
+
+    if prediction <0.3:
+        chance = "low"
+    elif prediction < 0.75:
+        chance = "moderate"
+    else:
+        chance = "high"
+
+    return json.dumps({
+        "prediction" : str(prediction[0][0]),
+        "likelihood": chance
+    })
+
 
 
 
---
+++ weatherAPI.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+import requests
+def makeCall(region):
+    load_dotenv()
+
+    key = os.getenv("WEATHER_API_KEY")
+    # Geocode region
+    geoUrl = f"http://api.openweathermap.org/geo/1.0/direct?q={region}&limit=1&appid={key}"
+    geoResponse = requests.get(geoUrl)
+
+    data = geoResponse.json()
+    lat = data[0]['lat']
+    lon = data[0]['lon']
+
+    #Get weather data
+    weatherUrl = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}"
+    weatherResponse = requests.get(weatherUrl)
+    weather = weatherResponse.json()
+    return weather['wind']['speed'], weather['rain']['1h']
+
+
