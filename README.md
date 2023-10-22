d4d17e57784e36a27ee7861b2faf2872a57739a9
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-22 20:37:32 +0530

    docs and prettify

--- README.md
+++ README.md
@@ -3,6 +3,22 @@
 A wildfire prediction API built in lieu with an IOT system.
 
 ## Pre-fire prediction endpoint
+- `/` - GET
+
+```landing page. say hi to the lorax```
+
+
+- `/predict` - POST
+```json
+{
+  "region": "string",
+    "date": "string", 
+    "temp": float,
+    "humidity": float  
+
+}
+```
+> Date should be in the format `YYYY-MM-DD`
 
 ### instructions
 - create a .env file in the root folder and add `WEATHER_API_KEY`
--- app.py
+++ app.py
@@ -13,22 +13,37 @@ model = load_model('MLModel/LoraxV1.1.h5')
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Lorax says hi'
-
-
-"""
-request json structure
-{
-    "region": string,
-    "date": string, //YYYY-MM-DD
-    "temp": float,
-    "humidity": float    
+    return """<h1>Lorax says hi!</h1>
+    request json structure <br>
+{ <br>
+    "region": string, <br>
+    "date": string, //YYYY-MM-DD <br>
+    "temp": float, <br>
+    "humidity": float  <br>  
 }
-"""
+<br>
+<br>
+Made by @Isuru2701 
+<br>
+check it out on github: <br>
+<a href='https://github.com/Isuru2701/Quokka'>Model </a> <br>
+<a href='https://github.com/Isuru2701/Lorax'>API </a>
+
+    """
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    """
+    request json structure
+    {
+        "region": string,
+        "date": string, //YYYY-MM-DD
+        "temp": float,
+        "humidity": float
+    }
+    """
+
     req = request.get_json()
     region = req['region']
     date = req['date']
@@ -42,12 +57,12 @@ def predict():
     print(datetime.strptime(date, '%Y-%m-%d'))
 
     year, month, day = date.split('-')
-    #month, day, temp, RH, wind, rain
+    # month, day, temp, RH, wind, rain
     print(int(month), dayoftheweek, temp, humidity, wind, rain)
     data = [int(month), dayoftheweek, temp, humidity, wind, rain]
     prediction = model.predict([data])
 
-    if prediction <0.3:
+    if prediction < 0.3:
         chance = "low"
     elif prediction < 0.75:
         chance = "moderate"
@@ -57,12 +72,9 @@ def predict():
         chance = "high"
 
     return json.dumps({
-        "prediction" : str(prediction[0][0]),
+        "prediction": str(prediction[0][0]),
         "likelihood": chance
     })
-
-
-
 
 
 if __name__ == '__main__':
